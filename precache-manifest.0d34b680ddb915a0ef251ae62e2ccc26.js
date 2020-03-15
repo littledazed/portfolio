6f7921e9e8b4af31f4864c9ef9df2bf5d4e118a4
Jinny Seo <jinnyseo@p202s-MacBook-Pro-97.local>
2020-03-15 15:53:18 -0500

    Updates

---
+++ precache-manifest.0d34b680ddb915a0ef251ae62e2ccc26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd005e775b48253c8ed11928b938723",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1f63807b4cff2d81ac07",
+    "url": "/portfolio/static/css/main.8baaa0c6.chunk.css"
+  },
+  {
+    "revision": "31d5b4351f6195d8c54d",
+    "url": "/portfolio/static/js/2.b6b9c8f2.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/portfolio/static/js/2.b6b9c8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f63807b4cff2d81ac07",
+    "url": "/portfolio/static/js/main.aafef1b0.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "09b773495df08c18c7064d7721770408",
+    "url": "/portfolio/static/media/placeholder.09b77349.jpg"
+  }
+]);
